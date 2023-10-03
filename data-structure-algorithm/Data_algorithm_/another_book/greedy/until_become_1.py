dfc805e40e11cc81b2ae60b3dcb4b9fe4447524b
leeyoungdong <lgg0025@naver.com>
2023-10-03 15:05:22 +0900

    1003-2

---
+++ data-structure-algorithm/Data_algorithm_/another_book/greedy/until_become_1.py
@@ -0,0 +1,32 @@
+# n, k = list(map(int, input().split()))
+
+# cnt = 0
+
+# while n != 1:
+#     if n % k == 0 :
+#         n = n // k
+#     else:
+#         n = n - 1
+#     cnt += 1
+
+# print(cnt)
+
+n, k = map(int, input().split())
+
+result = 0
+
+while True:
+    # N이 K로 나누어 떨어지는 수가 될 때까지만 1씩 빼기
+    target = (n // k) * k
+    result += (n - target)
+    n = target
+    # N이 K보다 작을 때 (더 이상 나눌 수 없을 때) 반복문 탈출
+    if n < k:
+        break
+    # K로 나누기
+    result += 1
+    n //= k
+
+# 마지막으로 남은 수에 대하여 1씩 빼기
+result += (n - 1)
+print(result)
