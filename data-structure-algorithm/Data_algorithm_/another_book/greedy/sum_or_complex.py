680dd0d5f3e68cad6fd973e2a9ec13fc31f3fbd2
leeyoungdong <lgg0025@naver.com>
2023-11-09 10:34:34 +0900

    1109

---
+++ data-structure-algorithm/Data_algorithm_/another_book/greedy/sum_or_complex.py
@@ -0,0 +1,15 @@
+data = input()
+# data = data[0]
+
+result = int(data[0])
+for i in range(1, len(data)):
+
+    num = int(data[i])
+
+    if num <= 1 or result <= 1:
+        result += num
+
+    else: 
+        result *= num
+
+print(result)
